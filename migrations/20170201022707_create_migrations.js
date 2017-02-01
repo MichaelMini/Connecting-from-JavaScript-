69b2d467771b77134b736d3e0aa6354fed85f819
MichaelMini <mcho911@gmail.com>
2017-02-01 03:10:33 +0000

    Create New Migration working.

---
+++ migrations/20170201022707_create_migrations.js
@@ -0,0 +1,13 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('milestones', function (table) {
+    table.increments();
+    table.string('description');
+    table.date('date_achieved');
+    table.integer('famous_person_id').references('id').inTable('famous_people');
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('milestones');
+};
